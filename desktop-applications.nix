b3731425f230baedad5ab1f4db654b25fcf49ac8
Simpaqt <simpa@protonmail.com>
2025-02-02 22:36:02 +0100

    update

--- desktop-applications.nix
+++ desktop-applications.nix
@@ -8,7 +8,6 @@
     spotify
     obs-studio
     google-chrome
-
   ];
 }
 
--- gaming.nix
+++ gaming.nix
@@ -5,7 +5,6 @@
   environment.systemPackages = with pkgs; [
     lutris
     gamemode
-    bottles
     wine
     protonup-qt
     wowup-cf
